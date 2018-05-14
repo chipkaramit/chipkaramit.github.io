fd7b12af477dcbc11f4622ee7e7245994ad78be6
Amit Chipkar <chipkaramit@gmail.com>
2018-05-14 15:03:59 +0530

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,16 @@
+{
+  "status": "success",
+  "error": {
+    "error_code": "12345",
+    "error_mesage": "some error message according to code"
+  },
+  "api_data": {
+    "background_image":[
+    "bg_image_360X480":"http://www.guoguiyan.com/data/out/84/69281704-gear-wallpapers.png",
+    "bg_image_720X1280":"https://wallpapersmug.com/download/720x1280/7a8434/sunrise-above-clouds-4k.jpg",
+    "bg_image_1280X1920":"https://www.publicdomainpictures.net/pictures/70000/velka/abstract-blurry-background.jpg"
+    ],
+    "text_color":"#ffffff",
+    "button_color":"#304ffe"
+  }
+}
